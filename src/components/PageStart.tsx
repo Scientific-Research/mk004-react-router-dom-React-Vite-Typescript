fd48c87d2cd4e3338b7f651e67fe480b1251653e
Maximilian <scientificresearch309@gmail.com>
2025-09-09 22:45:53 +0200

    an image is there on the start page

--- src/components/PageStart.tsx
+++ src/components/PageStart.tsx
@@ -1,7 +1,11 @@
+import europeanMap from '/src/assets/images/EuropeanMap.png';
+
 export const PageStart = () => {
   return (
-    <>
+    <div className="pageStart">
       <p> Welcome to this travel info site!</p>
-    </>
+
+      <img src={europeanMap} alt="" />
+    </div>
   );
 };
