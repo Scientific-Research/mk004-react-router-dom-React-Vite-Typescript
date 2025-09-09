0dc3f9f43cb95192a46e038198ae21d5b9840ef0
Maximilian <scientificresearch309@gmail.com>
2025-09-09 11:44:56 +0200

    all NavLink and Routes are there

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 // import './App.css';
 
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { PageGermany } from './components/PageGermany';
+import { PageFrance } from './components/PageFrance';
+import { PageSpain } from './components/PageSpain';
+
 function App() {
   return (
     <div className="App">
       <h1>Travel Info Site</h1>
       <p>Welcome to the Site!</p>
+
+      <nav>
+        <NavLink to="/germany">Germany</NavLink>
+        <NavLink to="/france">France</NavLink>
+        <NavLink to="/spain">Spain</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/germany" element={<PageGermany />}></Route>
+        <Route path="/france" element={<PageFrance />}></Route>
+        <Route path="spain" element={<PageSpain />}></Route>
+      </Routes>
     </div>
   );
 }
