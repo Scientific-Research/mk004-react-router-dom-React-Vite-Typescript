97df5ded52a039f24c4d2dda5ce9e67f4b145867
Maximilian <scientificresearch309@gmail.com>
2025-09-09 12:55:59 +0200

    three page => hannover, hamburg and Berlin created

--- src/components/PageGermany.tsx
+++ src/components/PageGermany.tsx
@@ -1,7 +1,26 @@
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import { PageHamburg } from './PageHamburg';
+import { PageHannover } from './PageHannover';
+import { PageBerlin } from './PageBerlin';
+
 export const PageGermany = () => {
-        return (
-            <>
-                <p>Welcome to the Germany page.</p>
-            </>
-        );
-    };
+  return (
+    <>
+      <nav>
+        <NavLink to="/hamburg">Hamburg</NavLink>
+        <NavLink to="/hannover">Hannover</NavLink>
+        <NavLink to="/berlin">Berlin</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/hamburg" element={<PageHamburg />}></Route>
+        <Route path="/hannover" element={<PageHannover />}></Route>
+        <Route path="/berlin" element={<PageBerlin />}></Route>
+        {/* <Route path="/" element={<PageGermany />}></Route> OR the following for the default page => even without replace*/}
+        <Route path="/" element={<Navigate to="/berlin" />}></Route>
+      </Routes>
+
+      <p>Welcome to the Germany page.</p>
+    </>
+  );
+};
