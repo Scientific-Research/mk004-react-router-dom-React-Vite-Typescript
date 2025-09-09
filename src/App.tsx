6b19891e5c1baaf40afc5264f35aaac86744eaf2
Maximilian <scientificresearch309@gmail.com>
2025-09-09 22:20:47 +0200

    The title has link now and clickable

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 // import './App.css';
 
-import {
-  BrowserRouter,
-  Navigate,
-  NavLink,
-  Route,
-  Routes,
-} from 'react-router-dom';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import { PageGermany } from './components/PageGermany';
 import { PageFrance } from './components/PageFrance';
 import { PageSpain } from './components/PageSpain';
@@ -15,13 +9,18 @@ import { PageStart } from './components/PageStart';
 function App() {
   return (
     <div className="App">
-      <h1>Travel Info Site</h1>
+      <h1>
+        <NavLink to="/">Travel Info Site</NavLink>
+      </h1>
+
       {/* <p>Welcome to the Site!</p> */}
+
       <nav>
         <NavLink to="germany">Germany</NavLink>
         <NavLink to="france">France</NavLink>
         <NavLink to="spain">Spain</NavLink>
       </nav>
+
       <Routes>
         <Route path="germany/*" element={<PageGermany />}></Route>
         <Route path="france/*" element={<PageFrance />}></Route>
