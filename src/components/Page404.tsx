e0171145d7c59ec85419f02693e1e5debbd22537
Maximilian <scientificresearch309@gmail.com>
2025-09-09 23:15:26 +0200

    page404 in => Page doesn't exist

--- src/components/Page404.tsx
+++ src/components/Page404.tsx
@@ -1,7 +1,13 @@
+import { useLocation } from 'react-router-dom';
+
 export const Page404 = () => {
+  const location = useLocation();
+  console.log(location);
   return (
-    <>
-      <h2>Page doesn't exist!</h2>
-    </>
+    <div className="page404">
+      <h2>
+        Page <code>{location.pathname}</code> doesn't exist!
+      </h2>
+    </div>
   );
 };
